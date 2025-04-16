6ff90b534423936703a58a97a86ea3bd52fdadcd
syvixor <syvixor@proton.me>
2025-04-16 15:33:24 +0100

    update: add card links

--- app/components/Popular.vue
+++ app/components/Popular.vue
@@ -6,7 +6,7 @@ const { data } = await useFetch("/api/popular", { query: { page: 1 } });
     <div class="space-y-4 p-4 lg:p-0">
         <h1 class="text-2xl font-bold">Most Popular</h1>
         <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 md:gap-4">
-            <NuxtLink v-for="(i, index) in data?.animes.slice(0, 6)" :key="i.id!"
+            <NuxtLink :to="`/info/${i.id}`" v-for="(i, index) in data?.animes.slice(0, 6)" :key="i.id!"
                 class="group rounded-xl transition-colors hover:bg-neutral-200 hover:dark:bg-neutral-800">
                 <div class="space-y-2 transition-transform group-hover:scale-90">
                     <div class="w-full h-60 md:h-80 lg:h-64">
